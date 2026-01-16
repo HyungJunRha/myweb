cdf750da7860e9e0696e11fe175cdca8ba6665d2
DESKTOP-LVN0GBM\user1 <HarryPotter>
2026-01-16 11:48:40 +0900

    회원가입 유효성 검증

--- app/index.js
+++ app/index.js
@@ -1,1 +1,40 @@
-console.log('zzz');
+function handleJoin(){
+
+    const userId = document.querySelector("input[name=userId]").value;
+    const userPw = document.querySelector("input[name=userPw]").value;
+    const userPw2 = document.querySelector("input[name=userPw2]").value;
+    const userNick = document.querySelector("input[name=userNick]").value;
+
+    //id (영어 소문자 및 숫자 허용, 4~8글자)
+    const idOk = /^[a-z0-9]{4,8}$/.test(userId);
+
+    //pw (숫자만 4~8)
+    const pwOk1 = /^[0-9]{4,8}$/.test(userPw);
+    const pwOk2 = userPw === userPw2;
+    const pwOk = pwOk1 && pwOk2;
+
+
+    //nick (한글만 4~8)
+    const nickOk = /^[가-힣]{4,8}$/.test(userNick);
+   
+   // id
+    if(!idOk){
+        alert('아이디 확인 바람');
+        return false;
+    }
+
+   // pw
+    if(!pwOk){
+        alert('비밀번호 확인 바람');
+        return false;
+    }
+
+   // nick
+    if(!nickOk){
+        alert('닉네임 확인 바람');
+        return false;
+    }
+
+
+    
+}
